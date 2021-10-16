af2048d32145aef12ea8affd0b3efac938a48b8a
macklark <ganesh.kolavennu@gmail.com>
2021-10-16 17:21:46 +0530

    added next-auth all catch api-route

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { passwordChecker } from "../../../lib/hasher";
+import { databaseConnection } from "../../../lib/helper";
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials) {
+        const client = await databaseConnection();
+
+        const user = await client.db().collection("users").findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          client.close();
+          throw new Error("User not found");
+        }
+
+        const isValid = await passwordChecker(
+          credentials.password,
+          user.hashPassword
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Invalid password");
+        }
+
+        client.close();
+
+        return {
+          email: user.email,
+        };
+      },
+    }),
+  ],
+});
