b68d0f0db93049c682d4e0b5a4c853372dbe013d
macklark <ganesh.kolavennu@gmail.com>
2021-10-23 12:42:16 +0530

    added new origin for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["m.media-amazon.com", "avatars.githubusercontent.com"],
+    domains: ["store.storeimages.cdn-apple.com"],
   },
 };
