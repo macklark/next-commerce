9c156e12f2d44f7d3ecb4d3a55e70fe08906bebc
macklark <ganesh.kolavennu@gmail.com>
2021-10-25 14:04:49 +0530

    added clerk/auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,36 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/nextjs";
+import { useRouter } from "next/router";
+
+const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[...index]]"];
 
 function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
+
+  const isPublicPage = publicPages.includes(pathname);
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ClerkProvider>
+      {isPublicPage ? (
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      ) : (
+        <ChakraProvider>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </ChakraProvider>
+      )}
+    </ClerkProvider>
   );
 }
 
