750b2f7ba5290a2c4d766905c31b704628b67de0
macklark <ganesh.kolavennu@gmail.com>
2021-12-21 16:16:37 +0530

    added responsive navbar at /

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+// Nextjs imports
+import Head from "next/head";
+
+// Component imports
+import Navbar from "../components/Navbar";
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Next Commerce</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <Navbar />
+  </>
+);
+
+export default Home;
