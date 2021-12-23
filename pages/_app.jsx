e6f8cf8e2cd63a0b111025892355024403a8fb17
macklark <ganesh.kolavennu@gmail.com>
2021-12-23 21:40:06 +0530

    added clerk authentication to higher level component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,39 @@
+// Nextjs imports
+import { useRouter } from "next/router";
+
 // Chakra provider
 import { ChakraProvider } from "@chakra-ui/react";
 
+// Clerk auth imports
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/nextjs";
+
+const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[...index]]"];
+
 function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
+  const isPublic = publicPages.includes(pathname);
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ClerkProvider>
+      {isPublic ? (
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      ) : (
+        <ChakraProvider>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </ChakraProvider>
+      )}
+    </ClerkProvider>
   );
 }
 
