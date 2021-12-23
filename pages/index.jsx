7da982d330cc0a7dd269415e02b2c510b03dc6f3
macklark <ganesh.kolavennu@gmail.com>
2021-12-23 07:15:01 +0530

    made layout more responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,8 +10,20 @@ import { Grid, GridItem, Box, Text, Button, Flex } from "@chakra-ui/react";
 
 const Layout = () => (
   <Box w={["100%", 11 / 12]} mx="auto" padding="4">
-    <Grid templateColumns="repeat(7, 1fr)" gap={4} h="200px">
-      <GridItem colSpan={2} bg="tomato" />
+    <Grid
+      templateColumns={{
+        base: "repeat(1, 1fr)",
+        lg: "repeat(7, 1fr)",
+        md: "repeat(3, 1fr)",
+      }}
+      gap={4}
+      h="200px"
+    >
+      <GridItem
+        colSpan={2}
+        bg="tomato"
+        display={{ base: "none", lg: "grid", md: "none" }}
+      />
       <GridItem colSpan={5}>
         <Grid
           templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
@@ -25,59 +37,10 @@ const Layout = () => (
               width="100%"
               height="70%"
               layout="responsive"
+              priority="low"
             />
             <Text fontSize="2xl" px="2" pt="2">
               Blue T-shirt
-            </Text>
-            <Text fontSize="lg" px="2">
-              $6.99
-            </Text>
-            <Flex justifyContent="center">
-              <Button
-                width="100%"
-                margin="2"
-                color="white"
-                bgGradient="linear(to-r,#7928CA, #FF0080 )"
-              >
-                Add to cart
-              </Button>
-            </Flex>
-          </GridItem>
-          <GridItem colSpan={1} rounded="md" boxShadow="base">
-            <Image
-              src="/redshirt.jpg"
-              alt="red shirt"
-              width="100%"
-              height="70%"
-              layout="responsive"
-            />
-            <Text fontSize="2xl" px="2" pt="2">
-              Red T-shirt
-            </Text>
-            <Text fontSize="lg" px="2">
-              $6.99
-            </Text>
-            <Flex justifyContent="center">
-              <Button
-                width="100%"
-                margin="2"
-                color="white"
-                bgGradient="linear(to-r,#7928CA, #FF0080 )"
-              >
-                Add to cart
-              </Button>
-            </Flex>
-          </GridItem>
-          <GridItem colSpan={1} rounded="md" boxShadow="base">
-            <Image
-              src="/greenshirt.jpg"
-              alt="green shirt"
-              width="100%"
-              height="70%"
-              layout="responsive"
-            />
-            <Text fontSize="2xl" px="2" pt="2">
-              Green T-shirt
             </Text>
             <Text fontSize="lg" px="2">
               $6.99
