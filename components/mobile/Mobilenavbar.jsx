54f11ca47844c85facd56b35266950527bbeff4a
macklark <ganesh.kolavennu@gmail.com>
2021-12-23 07:16:58 +0530

    added menu option for mobile responsive navbar

--- components/mobile/Mobilenavbar.jsx
+++ components/mobile/Mobilenavbar.jsx
@@ -1,17 +1,33 @@
 // Chakra imports
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Menu,
+  MenuList,
+  MenuItem,
+  Button,
+  MenuButton,
+} from "@chakra-ui/react";
 
 const Mobilenavbar = () => (
-  <Box display={{ base: "flex", md: "none" }}>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+  <Menu>
+    <MenuButton
+      display={{ base: "flex", md: "none" }}
+      cursor="pointer"
+      as={Button}
     >
-      <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
-    </svg>
-  </Box>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+      </svg>
+    </MenuButton>
+    <MenuList>
+      <MenuItem>Sign in</MenuItem>
+    </MenuList>
+  </Menu>
 );
 
 export default Mobilenavbar;
