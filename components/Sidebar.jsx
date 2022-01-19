08e62b66c620b582a0a7b9b3aa2f11d8ddfc17ac
macklark <ganesh.kolavennu@gmail.com>
2022-01-19 18:49:23 +0530

    removed unecessary options from Sidebar component

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -3,18 +3,19 @@ import { Box, Text } from "@chakra-ui/react";
 
 const Sidebar = () => (
   <Box p="5" rounded="md">
-    <Text fontSize="3xl" fontWeight="bold">
-      Filter
-    </Text>
     <Text fontSize="2xl" mt="5" fontWeight="semibold" color="facebook.300">
-      Price
+      Apperal
     </Text>
-    <Box mt="1">
-      <Text color="gray.600" fontSize="md" cursor="pointer">
-        Lowest to Largest
-      </Text>
-      <Text color="gray.600" fontSize="md" mt="1" cursor="pointer">
-        Largest to Lowest
+    <Box mt="3">
+      <Text
+        color="gray.600"
+        fontSize="md"
+        cursor="pointer"
+        padding="5px"
+        _hover={{ background: "gray.200", color: "black" }}
+        borderRadius="5px"
+      >
+        T-shirt's
       </Text>
     </Box>
   </Box>
