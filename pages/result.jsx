0b5713023e5f69fa8ed9c8f357615452c0b00b59
macklark <ganesh.kolavennu@gmail.com>
2022-01-23 10:46:34 +0530

    added /result

---
+++ pages/result.jsx
@@ -0,0 +1,87 @@
+import Navbar from "../components/Navbar";
+
+// Supabase imports
+import supabase from "../utils/supabaseClient";
+
+// Chakra imports
+import { Box, Grid, GridItem, Text, Flex, Button } from "@chakra-ui/react";
+
+// Nextjs imports
+import Image from "next/image";
+import Link from "next/link";
+
+export const getServerSideProps = async ({ query }) => {
+  const term = query.q;
+
+  let { data: products, error } = await supabase
+    .from("products")
+    .select()
+    .textSearch("name", term);
+
+  if (error) {
+    return {
+      props: {
+        error: error.message,
+      },
+    };
+  }
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
+const Result = ({ products }) => {
+  return (
+    <>
+      <Navbar />
+      <Box w={["100%", 11 / 12]} mx="auto" padding="4">
+        <Grid
+          templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(4, 1fr)" }}
+          gap={4}
+          h="200px"
+        >
+          {products.map((product) => (
+            <GridItem
+              colSpan={1}
+              rounded="md"
+              boxShadow="base"
+              key={product.id}
+            >
+              <Image
+                src={product.image}
+                alt={product.name}
+                width="100%"
+                height="70%"
+                layout="responsive"
+                priority="low"
+              />
+              <Text fontSize="2xl" px="2" pt="2">
+                {product.name}
+              </Text>
+              <Text fontSize="lg" px="2" mb="2">
+                ${product.price}
+              </Text>
+              <Flex justifyContent="center">
+                <Link href={"/products/" + product.id} passHref>
+                  <Button
+                    width="100%"
+                    color="white"
+                    m="1em"
+                    bgGradient="linear(to-r, #7928CA, #FF0080)"
+                  >
+                    Add to cart
+                  </Button>
+                </Link>
+              </Flex>
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default Result;
