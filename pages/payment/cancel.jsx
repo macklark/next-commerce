f58ee6db9abf411b4919b8b27d746e85544f2223
macklark <ganesh.kolavennu@gmail.com>
2022-02-05 12:14:54 +0530

    added /payment/cancel & /payment/success

---
+++ pages/payment/cancel.jsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Cancel = () => (
+  <Box>
+    <Text>Payment unsuccessful !!!</Text>
+  </Box>
+);
+
+export default Cancel;
---
+++ pages/payment/success.jsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Success = () => (
+  <Box>
+    <Text>Payment Successful !!!</Text>
+  </Box>
+);
+
+export default Success;
