9e0acbaaa8588cf77f68aac24a99040f5c7fcad9
macklark <ganesh.kolavennu@gmail.com>
2022-02-07 15:13:34 +0530

    made category links functional

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,22 +1,29 @@
 // Chakra imports
 import { Box, Text } from "@chakra-ui/react";
 
+// Nextjs imports
+import Link from "next/link";
+
 const Sidebar = () => (
   <Box p="5" rounded="md">
     <Text fontSize="2xl" mt="5" fontWeight="semibold" color="facebook.300">
       Apperal
     </Text>
     <Box mt="3">
-      <Text
-        color="gray.600"
-        fontSize="md"
-        cursor="pointer"
-        padding="5px"
-        _hover={{ background: "gray.200", color: "black" }}
-        borderRadius="5px"
-      >
-        T-shirts
-      </Text>
+      <Link href="/category">
+        <a>
+          <Text
+            color="gray.600"
+            fontSize="md"
+            cursor="pointer"
+            padding="5px"
+            _hover={{ background: "gray.200", color: "black" }}
+            borderRadius="5px"
+          >
+            T-shirts
+          </Text>
+        </a>
+      </Link>
     </Box>
   </Box>
 );
